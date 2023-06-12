d14661308e90ed311a67acb291c163a73c6e1e55
amanDev233 <108179513+amanDev233@users.noreply.github.com>
2023-06-12 13:03:05 +0530

    button fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from "axios";
+
 import "./App.css";
 
 class App extends React.Component {
   state = {
     advice: "",
   };
+
   componentDidMount() {
     this.fetchAdvice();
   }
@@ -14,7 +16,9 @@ class App extends React.Component {
     axios
       .get("https://api.adviceslip.com/advice")
       .then((response) => {
-        this.setState({ advice: response.data.slip.advice });
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
       })
       .catch((error) => {
         console.log(error);
@@ -22,14 +26,13 @@ class App extends React.Component {
   };
 
   render() {
-    const { advice } = this.state;
     return (
       <div className="app">
         <div className="card">
-          <h1 className="heading">{advice}</h1>
-          <buttton className="button" onclick={this.fetchAdvice()}>
-            <span>NEW ADVICE!</span>
-          </buttton>
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>NEW ADVICE</span>
+          </button>
         </div>
       </div>
     );
